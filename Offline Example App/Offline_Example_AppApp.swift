ef743fbd19b4b036564189f00de82878eec58283
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2026-03-21 14:29:09 -0300

    Initial Commit

---
+++ Offline Example App/Offline_Example_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Offline_Example_AppApp.swift
+//  Offline Example App
+//
+//  Created by Nicholas Affonso on 21/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Offline_Example_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
